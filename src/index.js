8a5c515745bcc222273443495099fe1eb256578b
Mohammed Manar Yazji <yazjimanar92@gmail.com>
2021-05-20 13:50:32 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,53 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 
-  // form and input values
-  const form = document.getElementById('create-task-form');
-  const taskList = document.getElementById('tasks');
+  const submitButton = document.getElementById("submitForm");
+  const tasks = document.getElementById("tasks");
+  const newTask = document.getElementById("new-task-description");
+  const removeTaks = document.getElementById("removeTask");
 
-
-  // Form submit event
-  form.addEventListener('submit', addTask);
-
-  // delete event
-  taskList.addEventListener('click', removeTask);
-
-  // Add task
-  function addTask(e){
+  submitButton.addEventListener("click", function(e){
+    let taskLi = document.createElement('li');
+    taskLi.textContent = newTask.value;
+    tasks.append(taskLi);
     e.preventDefault();
+  });
 
-    // get input value
-    const newTaskDescription = document.getElementById('new-task-description').value;
-    console.log(newTaskDescription);
-
-    // Create new li element
-    const li = document.createElement('li');
-
-    // Add text node with input value
-    li.appendChild(document.createTextNode(newTaskDescription));
-
-    // create delete button element
-    const deleteBtn = document.createElement('button');
-
-    // append text node to delete button
-    deleteBtn.appendChild(document.createTextNode('X'));
-
-    // append button to li
-    li.appendChild(deleteBtn);
-
-    // Append li to list
-    taskList.appendChild(li);
-
-  }
-
-  function removeTask(e) {
-    if (e.target.nodeName === 'BUTTON') {
-      if(confirm('Are you sure?')) {
-        let li = e.target.parentElement;
-        taskList.removeChild(li);
+  removeTaks.addEventListener("click", function(e){
+    const li = document.querySelectorAll();
+    for (let i = 0; i < li.length; i++){
+      if (newTask.value === li[i].textContent){
+        tasks.removeChild(li[i]);
       }
     }
+    e.preventDefault();
+  });
 
-  }
+
 
 });
